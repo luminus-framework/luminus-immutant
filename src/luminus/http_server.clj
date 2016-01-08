9283792c0113629180d7d454222fe61c975c5efa
Yogthos <dmitri.sotnikov@gmail.com>
2016-01-08 01:54:20 -0500

    API update

--- src/luminus/http_server.clj
+++ src/luminus/http_server.clj
@@ -1,16 +1,23 @@
 (ns luminus.http-server
-  (:require [clojure.tools.logging :as log]                        
+  (:require [clojure.tools.logging :as log]
             [immutant.web :as immutant]))
 
 (defonce http-server (atom nil))
 
-(defn start [handler init port]  
+(defn format-opts [opts]
+  (->> (dissoc opts :handler :init) (merge {:host "0.0.0.0"}) vec flatten))
+
+(defn start [{:keys [handler init port] :as opts}]
   (if @http-server
     (log/error "HTTP server is already running!")
-    (do
+    (try
       (init)
-      (reset! http-server (immutant/run handler :host "0.0.0.0" :port port))
-      (log/info "server started on port:" (:port @http-server)))))
+      (reset!
+       http-server
+       (apply immutant/run handler (format-opts opts)))
+      (log/info "server started on port:" (:port @http-server))
+      (catch Throwable t
+        (log/error t (str "server failed to start on port: " port))))))
 
 (defn stop [destroy]
   (when @http-server
@@ -18,4 +25,3 @@
     (immutant/stop @http-server)
     (reset! http-server nil)
     (log/info "HTTP server stopped")))
-
