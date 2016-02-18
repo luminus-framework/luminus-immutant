e6ba6f20e7b73b421adf37eb5721399595b125d6
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2016-02-18 17:13:25 -0500

    bumped up to latest immutant

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject luminus-immutant "0.1.0"
+(defproject luminus-immutant "0.1.1"
   :description "Immutant HTTP adapter for Luminus"
   :url "https://github.com/luminus-framework/luminus-immutant"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [org.immutant/web "2.1.1" :exclusions [ch.qos.logback/logback-classic]]])
+                 [org.immutant/web "2.1.2" :exclusions [ch.qos.logback/logback-classic]]])
