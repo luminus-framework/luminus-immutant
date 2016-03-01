5957313bd3070facdd6f1ab071df8bbd048b0c81
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2016-03-01 16:41:41 -0500

    cleanup for passing options

--- src/luminus/http_server.clj
+++ src/luminus/http_server.clj
@@ -2,13 +2,10 @@
   (:require [clojure.tools.logging :as log]
             [immutant.web :as immutant]))
 
-(defn format-opts [opts]
-  (->> (dissoc opts :handler) (merge {:host "0.0.0.0"}) vec flatten))
-
 (defn start [{:keys [handler port] :as opts}]
   (try
     (log/info "starting HTTP server on port" port)
-    (apply immutant/run handler (format-opts opts))
+    (immutant/run handler (->> (dissoc opts :handler) (merge {:host "0.0.0.0"}))
     (catch Throwable t
       (log/error t (str "server failed to start on port " port))
       (throw t))))
