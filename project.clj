1fd63fd37d224528e32df33aaaf0abeab4f86a21
Yogthos <dmitri.sotnikov@gmail.com>
2016-03-01 23:43:41 -0500

    added wrap-handler helper

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject luminus-immutant "0.1.7"
+(defproject luminus-immutant "0.1.8"
   :description "Immutant HTTP adapter for Luminus"
   :url "https://github.com/luminus-framework/luminus-immutant"
   :license {:name "Eclipse Public License"
--- src/luminus/http_server.clj
+++ src/luminus/http_server.clj
@@ -1,13 +1,18 @@
 (ns luminus.http-server
   (:require [clojure.tools.logging :as log]
-            [immutant.web :as immutant]))
+            [immutant.web :as immutant]
+            [clojure.set :refer [rename-keys]]))
 
 (defn run-options [opts]
   (merge
     {:host "0.0.0.0"}
-    (select-keys
-      (dissoc opts :contexts)
-      (-> #'immutant/run meta :valid-options))))
+    (-> opts
+        ;;options contain EVN path
+        ;;Immutant path is found at :handler-path
+        (dissoc :path :contexts)
+        (rename-keys {:handler-path :path})
+        (select-keys
+          (-> #'immutant/run meta :valid-options)))))
 
 (defn start [{:keys [handler port] :as opts}]
   (try
@@ -16,6 +21,9 @@
     (catch Throwable t
       (log/error t (str "server failed to start on port " port))
       (throw t))))
+
+(defn wrap-handler [server handler opts]
+  (immutant/run handler (merge server opts)))
 
 (defn stop [server]
   (immutant/stop server)
