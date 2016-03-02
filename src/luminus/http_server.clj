2d240afbd26f9138ecab58863dc3c5fd07be2842
Yogthos <dmitri.sotnikov@gmail.com>
2016-03-01 19:30:27 -0500

    removed conflicting config keys

--- src/luminus/http_server.clj
+++ src/luminus/http_server.clj
@@ -3,15 +3,16 @@
             [immutant.web :as immutant]))
 
 (defn run-options [opts]
-  (->>
+  (merge
+    {:host "0.0.0.0"}
     (select-keys
-      opts
-      (-> #'immutant/run meta :valid-options))
-    (merge {:host "0.0.0.0"})))
+      (dissoc opts :contexts :path)
+      (-> #'immutant/run meta :valid-options))))
 
 (defn start [{:keys [handler port] :as opts}]
   (try
     (log/info "starting HTTP server on port" port)
+    (clojure.pprint/pprint (run-options opts))
     (immutant/run handler (run-options opts))
     (catch Throwable t
       (log/error t (str "server failed to start on port " port))
