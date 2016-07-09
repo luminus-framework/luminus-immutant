0d7aff9c68e6d1b17774a6ddc930253686a021b9
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2016-07-09 02:00:31 -0400

    removed logback exclusion

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject luminus-immutant "0.2.1"
+(defproject luminus-immutant "0.2.2"
   :description "Immutant HTTP adapter for Luminus"
   :url "https://github.com/luminus-framework/luminus-immutant"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [org.immutant/web "2.1.5" :exclusions [ch.qos.logback/logback-classic]]])
+                 [org.immutant/web "2.1.5"]])
